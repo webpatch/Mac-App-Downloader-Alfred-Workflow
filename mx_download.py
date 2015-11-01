3ff8aa4705db5739e8f77bd19fb50b6483c49a00
kiny <kiny@kinydeMacBook-Pro.local>
2015-11-01 12:48:17 +0800

    add thunder download

--- mx_download.py
+++ mx_download.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import argparse
 from subprocess import call
 
 from lxml.etree import HTML
@@ -13,18 +14,28 @@ log = None
 
 
 def main(wf):
-    kw = wf.args[0]
-    id = kw.rsplit('/', 1)[-1].split('.')[0]
-    url = 'http://soft.macx.cn/downloado.do?softid={}&cpus=2&urls=3'.format(id)
-    r = web.get(url)
-    r.raise_for_status()
-    a = r.text
-    node = HTML(a).find('.//a[@rel="facebox"][last()]')
-    log.info(node.text)
-    if node is not None and node.text == '浏览器直接下载':
-        call(["open", node.get('href')])
-    else:
-        call(["open", url])
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--app', dest='app')
+    parse.add_argument('query', nargs='*', default=None)
+    args = parse.parse_args()
+    query = args.query[0]
+    log.warn(query)
+    if query:
+        id = query.rsplit('/', 1)[-1].split('.')[0]
+        url = 'http://soft.macx.cn/downloado.do?softid={}&cpus=2&urls=3'.format(id)
+        r = web.get(url)
+        r.raise_for_status()
+        a = r.text
+        node = HTML(a).find('.//a[@rel="facebox"][last()]')
+        log.info(node.text)
+        open = ['open']
+        if args.app:
+            open.extend(['-a',args.app])
+        if node is not None and node.text == '浏览器直接下载':
+            open.append(node.get('href'))
+        else:
+            open.append(url)
+        call(open)
 
 
 if __name__ == '__main__':
